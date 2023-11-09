f110414f781c40f60a0b6fe9cefea17af72cf8c3
NazriGenfil <abienazri23@gmail.com>
2023-11-09 13:10:48 +0700

    fix ga bisa headshot

--- damage.lua
+++ damage.lua
@@ -4,7 +4,24 @@ AddEventHandler('CEventGunShot', function (witnesses, ped, coords)
     
     if weaponsConfig then
         if weaponsConfig.disableCritical then
-            SetPedSuffersCriticalHits(playerPed, false)
+            -- SetPedSuffersCriticalHits(playerPed, false)
+            AddEventHandler('gameEventTriggered', function(name, args)
+                if name ~= 'CEventNetworkEntityDamage' then return end
+            
+                local victim = args[1]
+            
+                if playerPed == victim then
+                    local boneHit, whatBone = GetPedLastDamageBone(playerPed)
+            
+                    if boneHit then
+                        if whatBone == 31086 then
+                            if not IsEntityDead(playerPed) then
+                                SetEntityHealth(playerPed, 0)
+                            end	
+                        end
+                    end
+                end
+            end)            
         end
         N_0x4757f00bc6323cfe(weaponsConfig.model, weaponsConfig.modifier)
     end
