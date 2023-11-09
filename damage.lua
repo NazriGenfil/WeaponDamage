24668a0825deeb77fb011a0740a8cdf33a4faad9
Nazri <40690481+NazriGenfil@users.noreply.github.com>
2023-11-09 18:52:05 +0700

    cleaning code

--- damage.lua
+++ damage.lua
@@ -4,27 +4,32 @@ AddEventHandler('CEventGunShot', function (witnesses, ped, coords)
     
     if weaponsConfig then
         if weaponsConfig.disableCritical then
-            -- SetPedSuffersCriticalHits(playerPed, false)
-            AddEventHandler('gameEventTriggered', function(name, args)
-                if name ~= 'CEventNetworkEntityDamage' then return end
-            
-                local victim = args[1]
-            
-                if playerPed == victim then
-                    local boneHit, whatBone = GetPedLastDamageBone(playerPed)
-            
-                    if boneHit then
-                        if whatBone == 31086 then
-                            if not IsEntityDead(playerPed) then
-                                SetEntityHealth(playerPed, 0)
-                            end	
-                        end
-                    end
-                end
-            end)            
+            -- SetPedSuffersCriticalHits(playerPed, false)       
         end
         N_0x4757f00bc6323cfe(weaponsConfig.model, weaponsConfig.modifier)
     end
 end)
+
+AddEventHandler('gameEventTriggered', function(name, args)
+    local playerPed = PlayerPedId()
+    local weaponsConfig = Weapons[GetSelectedPedWeapon(playerPed)]
+    
+    if name ~= 'CEventNetworkEntityDamage' then return end
+    if not weaponsConfig.disableCritical then return end
+
+    local victim = args[1]
+
+    if playerPed == victim then
+        local boneHit, whatBone = GetPedLastDamageBone(playerPed)
+
+        if boneHit then
+            if whatBone == 31086 then
+                if not IsEntityDead(playerPed) then
+                    SetEntityHealth(playerPed, 0)
+                end	
+            end
+        end
+    end
+end)     
 
 print("Made by CURL with ❤️")
